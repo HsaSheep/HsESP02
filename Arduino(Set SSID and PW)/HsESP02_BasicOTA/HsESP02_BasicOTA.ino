7bf3ce0bcacba7570148858b2dbe0a0ff9e39e7a
Shogo Arato <hsasheep@gmail.com>
2017-10-22 13:29:24 +0900

    OTA用.ino追加とそれに伴うReadMe.mdの追記

--- Arduino(Set SSID and PW)/HsESP02_BasicOTA/HsESP02_BasicOTA.ino
+++ Arduino(Set SSID and PW)/HsESP02_BasicOTA/HsESP02_BasicOTA.ino
@@ -1,23 +1,13 @@
-/* /////////////////////////// */
-/*  BeepTest And MacAddr_Send  */
-/* 					            		   */
-/* SSIDandPW Write and Upload  */
-/* /////////////////////////// */
-
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <WiFiUdp.h>     //OTA
-#include <ArduinoOTA.h>  //OTA
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 //const char* ssid = "WiFi-SSID";
 //const char* password = "WiFi-Password";
 
 const char* ssid = "Buffalo-G-1D7A-Hs";
 const char* password = "xic7b5dfx3x8k";
-
-ESP8266WebServer server(80);
 
 const int led = 13;
 const int beep_pin =  16;
@@ -31,15 +21,7 @@ void beep(unsigned char delayms,unsigned char val = 20){
   digitalWrite(beep_pin, 0);
 }
 
-void tone_(unsigned int hz,unsigned int delayms = 100)
-{
-  tone(beep_pin, hz, delayms);
-  delay(delayms);
-  digitalWrite(beep_pin, 0);
-}
-
-char* mac_return(void)
-{
+void setup() {
   byte mac_addr[6];
   static char buff[20];
   WiFi.macAddress(mac_addr);
@@ -50,34 +32,7 @@ char* mac_return(void)
   mac_addr[3],
   mac_addr[4],
   mac_addr[5]);
-  return buff;
-}
 
-void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, 0);
-}
-
-void handleNotFound(){
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
-
-void setup(void){
-  //最低限のピン設定(LED,Beep等)OTA設定後にもう一回ピン設定
   pinMode(led, OUTPUT);
   pinMode(beep_pin,OUTPUT);
   digitalWrite(led, 1);
@@ -89,7 +44,7 @@ void setup(void){
   Serial.println("---- Booting... ---");
   Serial.println("----- Serial Begin -----");
   Serial.print("  Mac Addr: ");
-  Serial.println(mac_return());
+  Serial.println(buff);
   Serial.println(" !Enable OTA(BASIC_MODE)!");
   WiFi.mode(WIFI_STA); // OTA
   WiFi.begin(ssid, password);
@@ -111,11 +66,11 @@ void setup(void){
     if(counter > (60 * 5)) //5分以上経過
     {
       Serial.println("Connection Failed! Rebooting...");
-      digitalWrite(led,1);
+      digitalWrite(led, 1);
       delay(500);
-      digitalWrite(led,0);
+      digitalWrite(led, 0);
       delay(500);
-      digitalWrite(led,1);
+      digitalWrite(led, 1);
       delay(500);
       ESP.restart();
     }
@@ -161,47 +116,12 @@ void setup(void){
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  tone_(262,500);
-  tone_(523,500);
-
-  // Begin mDNS
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
-
-  // Server On
-  server.on("/", handleRoot);
-
-  server.on("/inline", [](){
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.onNotFound(handleNotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
-  tone_(2093,500);
-  tone_(4186,500);
+  beep(50);
+  beep(50);
   digitalWrite(led, 0);
-  Serial.println("----- !!! StartUp Done !!! -----");
+  Serial.println("----- !!! StartUp Done (OTA ONLY MODE) !!! -----");
 }
 
-
-void loop(void){
+void loop() {
   ArduinoOTA.handle(); // OTA
-  server.handleClient();
-  if(digitalRead(12) == LOW)
-  {
-    tone_(2093,150);
-    tone_(4186,300);
-
-    Serial.println("");
-    Serial.println("---------- ReSendStatus ----------");
-    Serial.print("  Mac Addr: ");
-    Serial.println(mac_return());
-    Serial.print(" Connected: ");
-    Serial.println(ssid);
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-  }
 }
